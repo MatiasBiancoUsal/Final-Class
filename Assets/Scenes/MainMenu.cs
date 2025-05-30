b8bb2dd25a856a6cfc745b292017dd4cd355336b
Braulio33 <v.trombetta1@usal.edu.ar>
2025-05-30 19:13:51 -0300

    Mis tareas están terminadas

---
+++ Assets/Scenes/MainMenu.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject settingsPanel;
+    public GameObject charactersPanel;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Prueba");
+    }
+
+    public void OpenSettings()
+    {
+        settingsPanel.SetActive(true);
+    }
+
+    public void OpenCharacters()
+    {
+        charactersPanel.SetActive(true);
+    }
+
+    public void ClosePanels()
+    {
+        settingsPanel.SetActive(false);
+        charactersPanel.SetActive(false);
+    }
+}
+
