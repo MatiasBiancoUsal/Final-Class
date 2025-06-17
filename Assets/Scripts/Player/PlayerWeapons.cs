59209506b4e913d9ebf20f0b66d04d6284295b14
Braulio33 <v.trombetta1@usal.edu.ar>
2025-06-17 09:53:51 -0300

    Actualizado,

--- Assets/Scripts/Player/PlayerWeapons.cs
+++ Assets/Scripts/Player/PlayerWeapons.cs
@@ -8,6 +8,9 @@ public class PlayerWeapons : MonoBehaviour
 
     private PlayerInventory _inventory;
     private AmmoHUD _hud;
+    [SerializeField] private AmmoHUD ammoHUD;
+
+    private PlayerInventory inv;
 
     private void Awake()
     {
@@ -50,5 +53,19 @@ public class PlayerWeapons : MonoBehaviour
         featherGunGO.SetActive(w == WeaponType.FeatherGun);
 
         _hud?.OnWeaponEquipped(w);   // refresca balas en pantalla
+
+
+        // Notificar al HUD
+        if (ammoHUD != null)
+        {
+            ammoHUD.OnWeaponEquipped(w);
+            Debug.Log("[PlayerWeapons] HUD actualizado para: " + w);
+        }
+        else
+        {
+            Debug.LogWarning("[PlayerWeapons] No se asignó AmmoHUD.");
+        }
+
+
     }
 }
