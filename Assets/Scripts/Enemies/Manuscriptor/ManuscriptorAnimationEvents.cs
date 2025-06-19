14be0c4fa57f7bc5caa716fb3812705318c7209f
Ninomax86 <constantinodacio.grau@usal.edu.ar>
2025-06-19 20:33:54 -0300

    enemigo2

---
+++ Assets/Scripts/Enemies/Manuscriptor/ManuscriptorAnimationEvents.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+// ManuscriptorAnimationEvents.cs
+using UnityEngine;
+
+public class ManuscriptorAnimationEvents : MonoBehaviour
+{
+    [Tooltip("Referencia al ManuscriptorHealth en el parent")]
+    public ManuscriptorHealth health;
+
+    [Tooltip("Referencia al ManuscriptorAI en el parent")]
+    public ManuscriptorAI ai;
+
+    public void OnDealMeleeDamage()
+        => ai.OnDealMeleeDamage();
+
+    public void OnMeleeAttackEnd()
+        => ai.OnMeleeAttackEnd();
+
+    public void OnDeathEnd()
+    {
+        if (health != null)
+            health.OnDeathAnimationEnd();
+    }
+}
