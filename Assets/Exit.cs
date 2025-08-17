93c5557d60947abbcfc0515b17d139e0826b1d68
MaximoAntonioBarbosaMoreira <m.barbosamoreira@usal.edu.ar>
2025-08-17 12:11:57 -0300

    exit te mueve de escena
    exit te mueve de escena

--- Assets/Exit.cs
+++ Assets/Exit.cs
@@ -1,8 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Exit : MonoBehaviour
 {
- 
+    GameObject player;
+    private void OnTriggerEnter(Collider other)
+    {
+        player = other.gameObject;
+
+        if (player.gameObject.tag == "Player")
+        {
+
+            SceneManager.LoadScene(1);
+
+
+
+        }
+    }
 }
