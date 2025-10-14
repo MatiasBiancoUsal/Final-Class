63afd18fa963e7650b1f86a3d0b55352beb2da31
Braulio33 <v.trombetta1@usal.edu.ar>
2025-10-14 12:01:37 -0300

    Archivo analytic

---
+++ Assets/Scenes/Analytic.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Services.Analytics;
+using Unity.Services.Core;
+
+public class Analitic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    async void Start()
+    {
+        await UnityServices.InitializeAsync();
+        AnalyticsService.Instance.StartDataCollection();
+    }
+
+}
